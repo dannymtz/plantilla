565060ec5753d588912f729d3958480fa322b8cb
daniel <daniel_mtz7@hotmail.com>
2023-03-29 11:48:10 -0600

    Se agrega la imagen del primer componente y los nombres de la invitacion

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,14 @@
+import foto from '../resources/Gallery_Photo_1.jpeg';
 
 function Title(props) {
     return(
-        <h1>{props.config.novia.nombre}</h1>
+        <div>
+            <h1 className="tipo_evento">{props.config.evento.tipoEvento}</h1>
+            <h1 className="nombres">
+                {props.config.novia.nombre} &amp; {props.config.novio.nombre}
+            </h1>
+            <img src={foto} ></img>
+        </div>
     );
 }
 
